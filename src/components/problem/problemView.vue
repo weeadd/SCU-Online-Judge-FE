8af2fb080b9a527921f12559b0e7fdcf94a470ed
1chrischen <2532901450@qq.com>
2024-05-22 09:12:23 +0800

    将顶部导航栏改成了要求（注意，userLogin里面有mounted函数，给gid赋了初值，应该删除掉，只有登陆成功之后才给gid赋值（学生1，老师2））

--- src/components/problem/problemView.vue
+++ src/components/problem/problemView.vue
@@ -1,16 +1,24 @@
 <template>
   <el-row style="margin: auto;max-width: 1500px;min-width: 600px;">
-    <el-col :xs="24" :sm="24" :md="18">
+    <el-col :xs="24" :sm="24" :md="12">
       <el-card class="box-card" shadow="hover">
         <template #header>
           <div class="card-header">
             <p class="title">#{{ problemInfo.pid }}、{{ problemInfo.title }}</p>
           </div>
         </template>
-        <div v-if="isSubmit">
+        <v-md-preview v-show="!isSubmit" :text="problemInfo.description"> </v-md-preview>
+      </el-card>
+    </el-col>
+
+
+    <el-col :xs="24" :sm="24" :md="12">
+      <el-card class="box-card" shadow="hover">
+
+        <div v-if="!isSubmit">
           <monacoEditor :value="code" @update:value="code = $event" />
           <el-divider />
-      <div style="text-align: center;">
+          <div style="text-align: center;">
             <el-button type="primary" @click="submit">
               <el-icon class="el-icon--left">
                 <Upload />
@@ -19,41 +27,8 @@
             </el-button>
           </div>
         </div>
-        <v-md-preview v-show="!isSubmit" :text="problemInfo.description"> </v-md-preview>
-      </el-card>
-    </el-col>
-    <el-col :xs="24" :sm="24" :md="6">
-      <el-card class="box-card" shadow="hover">
-        <template #header>
-          <div class="card-header">
-            题目信息 (提交: {{ problemInfo.submitCnt }} AC: {{ problemInfo.acCnt }})
-          </div>
-        </template>
-        <el-descriptions direction="vertical" :column="1" border>
-          <el-descriptions-item label="时间限制"> {{ problemInfo.timeLimit }} ms</el-descriptions-item>
-          <el-descriptions-item label="空间限制"> {{ problemInfo.memoryLimit }} MB</el-descriptions-item>
-          <el-descriptions-item label="题目类型"> {{ problemInfo.type }} </el-descriptions-item>
-<!--          <el-descriptions-item label=" 出题人">
-            <span class="rlink" @click="this.$router.push('/user/' + problemInfo.publisherUid)"> {{
-              problemInfo.publisher
-            }}</span>
-          </el-descriptions-item>-->
-          <el-descriptions-item label="发布时间"> {{ problemInfo.time }} </el-descriptions-item>
-        </el-descriptions>
-        <el-divider style="margin-top: 20px; margin-bottom: 20px;" />
+
         <div style="text-align: center;">
-          <el-button v-if="!this.isSubmit" type="primary" @click="this.isSubmit = true">
-            <el-icon class="el-icon--left">
-              <Upload />
-            </el-icon>
-            提交代码
-          </el-button>
-          <el-button v-if="this.isSubmit" type="success" @click="this.isSubmit = false">
-            <el-icon class="el-icon--left">
-              <RefreshLeft />
-            </el-icon>
-            返回题目
-          </el-button>
           <el-button v-if="this.gid > 1" type="danger" @click="this.$router.push('/problem/edit/' + problemInfo.pid)">
             <el-icon class="el-icon--left">
               <Operation />
@@ -63,6 +38,8 @@
         </div>
       </el-card>
     </el-col>
+
+
   </el-row>
 </template>
 
